1c8d208c2cd15cd40acb8b1cd74e1f9dd513cee5
theod <theodor.adam88@gmail.com>
2025-03-13 18:13:27 +0100

    Added model and get model for transformations

--- src/solid/Solid.java
+++ src/solid/Solid.java
@@ -2,6 +2,8 @@ package solid;
 
 import model.Vertex;
 import model.Part;
+import transforms.Mat4;
+import transforms.Mat4Identity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,7 @@ public abstract class Solid {
     protected List <Integer> indexBuffer = new ArrayList<Integer>();
     protected List<Vertex> vertexBuffer = new ArrayList<Vertex>();
     protected List <Part> partBuffer = new ArrayList<Part>();
+    protected Mat4 model = new Mat4Identity();
 
 
     public List<Integer> getIndexBuffer() {
@@ -21,5 +24,8 @@ public abstract class Solid {
     }
     public List<Part> getPartBuffer() {
         return partBuffer;
+    }
+    public Mat4 getModel() {
+        return model;
     }
 }
