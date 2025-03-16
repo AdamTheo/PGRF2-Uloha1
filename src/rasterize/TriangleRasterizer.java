ce4b727a89c1cdf6db4f482fa0a6ca7b5f3a1621
theod <theodor.adam88@gmail.com>
2025-03-16 16:38:44 +0100

    Finished triangleRasterizer with culling

--- src/rasterize/TriangleRasterizer.java
+++ src/rasterize/TriangleRasterizer.java
@@ -10,26 +10,21 @@ public class TriangleRasterizer {
     LineRasterizer lineRasterizer;
     ZBuffer buffer;
     Lerp lerp;
+    int maxWidth;
+    int maxHeight;
 
     public TriangleRasterizer(LineRasterizer lineRasterizer, ZBuffer buffer) {
         this.lineRasterizer = lineRasterizer;
         this.buffer = buffer;
         this.lerp = new Lerp();
+        maxWidth = buffer.getWidth() ;
+        maxHeight = buffer.getHeight();
     }
     public void rasterize(Vertex a, Vertex b, Vertex c) {
-        //(int)a.getPoint().getX(),(int)a.getPoint().getY()
-        //lineRasterizer.drawLine(new Line((int)a.getPoint().getX(),(int)a.getPoint().getY(),(int)b.getPoint().getX(),(int)b.getPoint().getY()));
-        //lineRasterizer.drawLine(new Line((int)b.getPoint().getX(),(int)b.getPoint().getY(),(int)c.getPoint().getX(),(int)c.getPoint().getY()));
-        //lineRasterizer.drawLine(new Line((int)a.getPoint().getX(),(int)a.getPoint().getY(),(int)c.getPoint().getX(),(int)c.getPoint().getY()));
-
 
         Vertex A = new Vertex(a);
         Vertex B = new Vertex(b);
         Vertex C = new Vertex(c);
-
-
-        //TODO: A MUSI BYT NEJMENSI, PAK B, PAK C
-
 
         if(A.getPoint().getY() > B.getPoint().getY()) {
             Vertex tmp = new Vertex(A);
@@ -52,21 +47,12 @@ public class TriangleRasterizer {
         int yB =(int)Math.round(B.getPoint().getY());
         int yC =(int)Math.round(C.getPoint().getY());
 
-        System.out.println(yA + " " + yB + " " + yC);
-
-        for(int y = yA; y < yB; y++) {
+        for(int y = Math.max(yA,1); y < Math.min(yB,maxWidth); y++) {
             double tAB = ((double)y-yA)/((double)yB-yA);
-            //int x1= (int)Math.round((1-tAB)*xA + tAB*xB);
             Vertex a1 = ((Vertex)lerp.lerp(A,B,tAB));
             double tAC = (double)(y - yA)/((double)yC-yA);
-            //int x2= (int)Math.round((1-tAC)*xA + tAC*xC);
             Vertex a2 = ((Vertex)lerp.lerp(A,C,tAC));
 
-           /*if(x2 < x1){
-               int tmp = x2;
-               x2 = x1;
-               x1 = tmp;
-           }*/
            if(a1.getPoint().getX() > a2.getPoint().getX()) {
                Vertex tmp = new Vertex(a1);
                a1 = new Vertex(a2);
@@ -76,37 +62,22 @@ public class TriangleRasterizer {
            int x1 = (int) Math.round(a1.getPoint().getX());
            int x2 = (int) Math.round(a2.getPoint().getX());
 
-            for(int x = x1; x < x2; x++) {
-                //TODO: OREZANI HLIDAT OKNO OBRAZOVKY
-                double z1 = a1.getPoint().getZ();
-                double z2 = a2.getPoint().getZ();
-
-                double tz = (double)(y-a1.getPoint().getY())/((double)a2.getPoint().getY() - a1.getPoint().getY());
+            for(int x = Math.max(x1,1); x < Math.min(x2,maxWidth); x++) {
+                double tz = (double)(x-x1)/(double)(x2-x1);
                 Vertex z3 = ((Vertex)lerp.lerp(a1,a2,tz));
-                double z = z3.getPoint().getZ();
-                System.out.println(z);
-
-
-
-                buffer.setPixelWithZTest(x,y,0.1,new Col(0xffff00));
+                buffer.setPixelWithZTest(x,y,z3.getPoint().getZ(),z3.getColor());
             }
         }
 
         // DRUHA CAST
 
-        for(int y = yB; y < yC; y++) {
+        for(int y = Math.max(yB,1); y < Math.min(yC, maxHeight); y++) {
             double tBC = ((double)y-yB)/((double)yC-yB);
-            //int x1 =(int)Math.round((1-tBC)*xB + tBC*xC);
             Vertex b1 = ((Vertex)lerp.lerp(B,C,tBC));
 
             double tAC = (double)(y - yA)/((double)yC-yA);
             Vertex b2 = ((Vertex)lerp.lerp(A,C,tAC));
 
-            /*if(x2 < x1){
-                int tmp = x2;
-                x2 = x1;
-                x1 = tmp;
-            }*/
             if(b1.getPoint().getX() > b2.getPoint().getX()) {
                 Vertex tmp = new Vertex(b1);
                 b1 = new Vertex(b2);
@@ -115,9 +86,10 @@ public class TriangleRasterizer {
             int x1 = (int) Math.round(b1.getPoint().getX());
             int x2 = (int) Math.round(b2.getPoint().getX());
 
-            for(int x = x1; x < x2; x++) {
-                //TODO: OREZANI HLIDAT OKNO OBRAZOVKY
-                buffer.setPixelWithZTest(x,y,0.1,new Col(0xffff00));
+            for(int x = Math.max(x1,1); x < Math.min(x2,maxWidth); x++) {
+                double tz = (double)(x-x1)/(double)(x2-x1);
+                Vertex z3 = ((Vertex)lerp.lerp(b1,b2,tz));
+                buffer.setPixelWithZTest(x,y,z3.getPoint().getZ(),z3.getColor());
             }
 
         }
