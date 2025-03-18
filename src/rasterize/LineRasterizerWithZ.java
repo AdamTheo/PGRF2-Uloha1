9e2a8ae0018c4703b571862ef226b7e5133d26c4
theod <theodor.adam88@gmail.com>
2025-03-18 14:56:32 +0100

    Implemented shader to rasterizers

--- src/rasterize/LineRasterizerWithZ.java
+++ src/rasterize/LineRasterizerWithZ.java
@@ -3,6 +3,8 @@ package rasterize;
 import model.Vertex;
 import raster.Raster;
 import raster.ZBuffer;
+import shader.Shader;
+import shader.ShaderInterpolated;
 import utils.Lerp;
 
 public class LineRasterizerWithZ {
@@ -10,85 +12,81 @@ public class LineRasterizerWithZ {
     private Lerp lerp;
     int maxWidth;
     int maxHeight;
+    Shader shader;
+
     public LineRasterizerWithZ(ZBuffer buffer) {
         this.buffer = buffer;
         lerp = new Lerp();
-        maxWidth = buffer.getWidth() -1 ; //This is needed to decrease by one to due to nature of algorithm and my implementation
-        maxHeight = buffer.getHeight() -1;
+        maxWidth = buffer.getWidth() - 1; //This is needed to decrease by one to due to nature of algorithm and my implementation
+        maxHeight = buffer.getHeight() - 1;
+        shader = new ShaderInterpolated();
     }
 
     public void rasterize(Vertex a1, Vertex b1) {
-            Vertex a = new Vertex(a1);
-            Vertex b = new Vertex(b1);
+        Vertex a = new Vertex(a1);
+        Vertex b = new Vertex(b1);
 
+        //Using trivial algorith with lerp interpolation, just checking if line is not vertical or if we should iterate by x or y, otherwise its just interpolation.
 
-            //Using trivial algorith with lerp interpolation, just checking if line is not vertical or if we should iterate by x or y, otherwise its just interpolation.
+        if (a.getPoint().getX() == b.getPoint().getX()) { // Using trivial algorithm, this is needed otherwise in K calculation dividing by zero could happen
+            if (b.getPoint().getY() < a.getPoint().getY()) {
+                Vertex tmp = a;
+                a = new Vertex(b);
+                b = new Vertex(tmp);
+            }
+            int y1 = (int) Math.round(a.getPoint().getY());
+            int y2 = (int) Math.round(b.getPoint().getY());
+            for (int y = Math.max(y1, 1); y <= Math.min(y2, maxHeight); y++) {
+                double t = ((double) y - y1) / ((double) y2 - y1);
+                Vertex v = (Vertex) lerp.lerp(a, b, t);
+                if (v.getPoint().getX() <= maxWidth && v.getPoint().getX() > 0) { //Check for X coordinate in raster
+                    buffer.setPixelWithZTest((int) Math.round(v.getPoint().getX()), (int) Math.round(v.getPoint().getY()), v.getPoint().getZ(), shader.getColor(v));
+                }
+            }
+        } else {
 
+            // if line IS NOT vertical
+            double k = (b.getPoint().getY() - a.getPoint().getY()) / (b.getPoint().getX() - a.getPoint().getX());
+            if (k > -1 && k < 1) {
+                if (a.getPoint().getX() > b.getPoint().getX()) {
+                    Vertex tmp = new Vertex(a);
+                    a = new Vertex(b);
+                    b = new Vertex(tmp);
+                }
+                int x1 = (int) Math.round(a.getPoint().getX());
+                int x2 = (int) Math.round(b.getPoint().getX());
 
-
-            if (a.getPoint().getX() == b.getPoint().getX()) { // Using trivial algorithm, this is needed otherwise in K calculation dividing by zero could happen
+                for (int x = Math.max(1, x1); x <= Math.min(maxWidth, x2); x++) {
+                    double t = ((double) x - x1) / ((double) x2 - x1);
+                    Vertex v = (Vertex) lerp.lerp(a, b, t);
+                    if (v.getPoint().getY() <= maxHeight && v.getPoint().getY() > 0) {  //Check for Y coordinate in raster
+                        buffer.setPixelWithZTest((int) Math.round(v.getPoint().getX()), (int) Math.round(v.getPoint().getY()), v.getPoint().getZ(), shader.getColor(v));
+                    }
+                }
+            } else {
+                // Using trivial algorithm
                 if (b.getPoint().getY() < a.getPoint().getY()) {
                     Vertex tmp = a;
                     a = new Vertex(b);
                     b = new Vertex(tmp);
                 }
-                int y1 = (int)Math.round(a.getPoint().getY());
-                int y2 = (int)Math.round(b.getPoint().getY());
-                for (int y = Math.max(y1,1); y <= Math.min(y2,maxHeight); y++) {
-                    double t = ((double)y-y1)/((double)y2-y1);
-                    Vertex v = (Vertex)lerp.lerp(a,b,t);
-                    if(v.getPoint().getX() <=  maxWidth && v.getPoint().getX() > 0 ) {
-                        buffer.setPixelWithZTest((int)Math.round(v.getPoint().getX()),(int)Math.round(v.getPoint().getY()),v.getPoint().getZ(),v.getColor());
+                int y1 = (int) Math.round(a.getPoint().getY());
+                int y2 = (int) Math.round(b.getPoint().getY());
+                for (int y = Math.max(y1, 1); y <= Math.min(y2, maxHeight); y++) {
+                    double t = ((double) y - y1) / ((double) y2 - y1);
+                    Vertex v = (Vertex) lerp.lerp(a, b, t);
+                    if (v.getPoint().getX() <= maxWidth && v.getPoint().getX() > 0) { //Check for X coordinate in raster
+                        buffer.setPixelWithZTest((int) Math.round(v.getPoint().getX()), (int) Math.round(v.getPoint().getY()), v.getPoint().getZ(), shader.getColor(v));
                     }
                 }
-            } else {
-
-                // if line IS NOT vertical
-                double k = (b.getPoint().getY() - a.getPoint().getY()) / (b.getPoint().getX() - a.getPoint().getX());
-                if (k > -1 && k < 1) {
-                    if(a.getPoint().getX() > b.getPoint().getX()) {
-                        Vertex tmp = new Vertex(a);
-                        a = new Vertex(b);
-                        b = new Vertex(tmp);
-                    }
-                    int x1 = (int)Math.round(a.getPoint().getX());
-                    int x2 = (int)Math.round(b.getPoint().getX());
-
-                    for (int x = Math.max(1,x1); x <= Math.min(maxWidth,x2); x++) {
-                        double t = ((double)x-x1)/((double)x2-x1);
-                        Vertex v = (Vertex)lerp.lerp(a,b,t);
-                        if(v.getPoint().getY() <= maxHeight && v.getPoint().getY() > 0 ) {
-                            buffer.setPixelWithZTest((int)Math.round(v.getPoint().getX()),(int)Math.round(v.getPoint().getY()),v.getPoint().getZ(),v.getColor());
-                        }
 
 
-                    }
-                } else {
-                     // Using trivial algorithm
-                        if (b.getPoint().getY() < a.getPoint().getY()) {
-                            Vertex tmp = a;
-                            a = new Vertex(b);
-                            b = new Vertex(tmp);
-                        }
-                        int y1 = (int)Math.round(a.getPoint().getY());
-                        int y2 = (int)Math.round(b.getPoint().getY());
-                        for (int y = Math.max(y1,1); y <= Math.min(y2,maxHeight); y++) {
-                            double t = ((double)y-y1)/((double)y2-y1);
-                            Vertex v = (Vertex)lerp.lerp(a,b,t);
-                            if(v.getPoint().getX() <=  maxWidth && v.getPoint().getX() > 0 ) {
-                                buffer.setPixelWithZTest((int)Math.round(v.getPoint().getX()),(int)Math.round(v.getPoint().getY()),v.getPoint().getZ(),v.getColor());
-                            }
-                        }
-
-
-                    }
-                }
             }
-
-
-
-
+        }
     }
+
+
+}
 
 
 
--- src/rasterize/TriangleRasterizer.java
+++ src/rasterize/TriangleRasterizer.java
@@ -5,6 +5,8 @@ import model.Vertex;
 import raster.ZBuffer;
 import transforms.Col;
 import utils.Lerp;
+import shader.Shader;
+import shader.ShaderInterpolated;
 
 public class TriangleRasterizer {
     LineRasterizer lineRasterizer;
@@ -12,87 +14,89 @@ public class TriangleRasterizer {
     Lerp lerp;
     int maxWidth;
     int maxHeight;
+    Shader shader;
 
     public TriangleRasterizer(LineRasterizer lineRasterizer, ZBuffer buffer) {
         this.lineRasterizer = lineRasterizer;
         this.buffer = buffer;
         this.lerp = new Lerp();
-        maxWidth = buffer.getWidth() ;
+        maxWidth = buffer.getWidth();
         maxHeight = buffer.getHeight();
+        shader = new ShaderInterpolated(); //Basic shader that we are going to use
     }
+
     public void rasterize(Vertex a, Vertex b, Vertex c) {
 
         Vertex A = new Vertex(a);
         Vertex B = new Vertex(b);
         Vertex C = new Vertex(c);
-
-        if(A.getPoint().getY() > B.getPoint().getY()) {
+        //Sort vertexes by Y
+        if (A.getPoint().getY() > B.getPoint().getY()) {
             Vertex tmp = new Vertex(A);
             A = new Vertex(B);
             B = new Vertex(tmp);
         }
 
-        if(A.getPoint().getY() > C.getPoint().getY()) {
+        if (A.getPoint().getY() > C.getPoint().getY()) {
             Vertex tmp = new Vertex(A);
             A = new Vertex(C);
             C = new Vertex(tmp);
         }
-        if(B.getPoint().getY() > C.getPoint().getY()) {
+        if (B.getPoint().getY() > C.getPoint().getY()) {
             Vertex tmp = new Vertex(B);
             B = new Vertex(C);
             C = new Vertex(tmp);
         }
 
-        int yA =(int)Math.round(A.getPoint().getY());
-        int yB =(int)Math.round(B.getPoint().getY());
-        int yC =(int)Math.round(C.getPoint().getY());
+        int yA = (int) Math.round(A.getPoint().getY());
+        int yB = (int) Math.round(B.getPoint().getY());
+        int yC = (int) Math.round(C.getPoint().getY());
+        // Interpolation of x coordinates
+        for (int y = Math.max(yA, 1); y < Math.min(yB, maxHeight); y++) {
+            double tAB = ((double) y - yA) / ((double) yB - yA);
+            Vertex a1 = ((Vertex) lerp.lerp(A, B, tAB));
+            double tAC = (double) (y - yA) / ((double) yC - yA);
+            Vertex a2 = ((Vertex) lerp.lerp(A, C, tAC));
 
-        for(int y = Math.max(yA,1); y < Math.min(yB,maxHeight); y++) {
-            double tAB = ((double)y-yA)/((double)yB-yA);
-            Vertex a1 = ((Vertex)lerp.lerp(A,B,tAB));
-            double tAC = (double)(y - yA)/((double)yC-yA);
-            Vertex a2 = ((Vertex)lerp.lerp(A,C,tAC));
+            if (a1.getPoint().getX() > a2.getPoint().getX()) {
+                Vertex tmp = new Vertex(a1);
+                a1 = new Vertex(a2);
+                a2 = new Vertex(tmp);
+            }
 
-           if(a1.getPoint().getX() > a2.getPoint().getX()) {
-               Vertex tmp = new Vertex(a1);
-               a1 = new Vertex(a2);
-               a2 = new Vertex(tmp);
-           }
-
-           int x1 = (int) Math.round(a1.getPoint().getX());
-           int x2 = (int) Math.round(a2.getPoint().getX());
-
-            for(int x = Math.max(x1,1); x < Math.min(x2,maxWidth); x++) {
-                double tz = (double)(x-x1)/(double)(x2-x1);
-                Vertex z3 = ((Vertex)lerp.lerp(a1,a2,tz));
-                buffer.setPixelWithZTest(x,y,z3.getPoint().getZ(),z3.getColor());
+            int x1 = (int) Math.round(a1.getPoint().getX());
+            int x2 = (int) Math.round(a2.getPoint().getX());
+            //Interpolation of vertex on each x between x1 and x2
+            for (int x = Math.max(x1, 1); x < Math.min(x2, maxWidth); x++) {
+                double tz = (double) (x - x1) / (double) (x2 - x1);
+                Vertex z3 = ((Vertex) lerp.lerp(a1, a2, tz));
+                buffer.setPixelWithZTest(x, y, z3.getPoint().getZ(), shader.getColor(z3));
             }
         }
 
         // DRUHA CAST
 
-        for(int y = Math.max(yB,1); y < Math.min(yC, maxHeight); y++) {
-            double tBC = ((double)y-yB)/((double)yC-yB);
-            Vertex b1 = ((Vertex)lerp.lerp(B,C,tBC));
+        // Interpolation of x coordinates
+        for (int y = Math.max(yB, 1); y < Math.min(yC, maxHeight); y++) {
+            double tBC = ((double) y - yB) / ((double) yC - yB);
+            Vertex b1 = ((Vertex) lerp.lerp(B, C, tBC));
 
-            double tAC = (double)(y - yA)/((double)yC-yA);
-            Vertex b2 = ((Vertex)lerp.lerp(A,C,tAC));
+            double tAC = (double) (y - yA) / ((double) yC - yA);
+            Vertex b2 = ((Vertex) lerp.lerp(A, C, tAC));
 
-            if(b1.getPoint().getX() > b2.getPoint().getX()) {
+            if (b1.getPoint().getX() > b2.getPoint().getX()) {
                 Vertex tmp = new Vertex(b1);
                 b1 = new Vertex(b2);
                 b2 = new Vertex(tmp);
             }
             int x1 = (int) Math.round(b1.getPoint().getX());
             int x2 = (int) Math.round(b2.getPoint().getX());
-
-            for(int x = Math.max(x1,1); x < Math.min(x2,maxWidth); x++) {
-                double tz = (double)(x-x1)/(double)(x2-x1);
-                Vertex z3 = ((Vertex)lerp.lerp(b1,b2,tz));
-                buffer.setPixelWithZTest(x,y,z3.getPoint().getZ(),z3.getColor());
+            //Interpolation of vertex on each x between x1 and x2
+            for (int x = Math.max(x1, 1); x < Math.min(x2, maxWidth); x++) {
+                double tz = (double) (x - x1) / (double) (x2 - x1);
+                Vertex z3 = ((Vertex) lerp.lerp(b1, b2, tz));
+                buffer.setPixelWithZTest(x, y, z3.getPoint().getZ(), shader.getColor(z3));
             }
-
         }
-
     }
 }
